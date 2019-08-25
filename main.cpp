839e89061406e67e848194f9f5f116b54083c6eb
cesar214567 <cesar.madera@utec.edu.pe>
2019-08-25 16:45:28 -0500

    done

--- main.cpp
+++ main.cpp
@@ -1,7 +1,38 @@
+#include <string>
+#include <string.h>
+#include <vector>
+#include <map>
 #include <iostream>
+#include <iterator>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    return 0;
+
+int main(){
+    map<string,int> mapa;
+
+
+    string palabra;
+    int cantidad_de_veces;
+    cin>>cantidad_de_veces;
+    for (int i=0;i<cantidad_de_veces;i++){
+        cin>>palabra;
+        if(mapa.find(palabra) != mapa.end()){
+
+            mapa[palabra]+=1;
+        }else{
+            mapa.insert(make_pair(palabra,1));
+
+        }
+        getline(cin,palabra);
+
+    }
+    auto iterator=mapa.begin();
+    while(iterator != mapa.end())
+    {
+        cout<<iterator->first<<" "<<iterator->second<<std::endl;
+        iterator++;
+    }
+
+
 }
